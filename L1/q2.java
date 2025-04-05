290fbd294de52ad81255057d7f2e961e362937d7
Ranjan <kumarranjansn401@gamil.com>
2025-04-06 04:03:16 +0530

    L1_q2

--- L1/q2.java
+++ L1/q2.java
@@ -1,3 +1,18 @@
+// * Question:Get a number from user and subtract 5 to that number and print the
+// result.
+// Example:Input :45 Output 40. Input:56789 Output:56784
+
+import java.util.Scanner;
+
 public class q2 {
+    public static void main(String[] args) {
+       Scanner input =new Scanner(System.in);
+        System.out.println("Enter a subtract number:");
+        int num = input.nextInt();
+        int sub= num-5;
+        System.out.println(sub);
+       
+        input.close();
+    }
     
 }
